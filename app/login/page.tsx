4dd0a3da8d21c1848ef8bfc3e18f13fde4bb0e06
alinaj04 <alinajames04@gmail.com>
2025-12-02 16:29:35 -0500

    Updated Login Page fixed formating. AJ

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,24 +4,27 @@ import React from "react";
 import {Button, Input, Checkbox, Link, Form, Divider} from "@heroui/react";
 import {Icon} from "@iconify/react";
 
-export default function Component() {
-    const [isVisible, setIsVisible] = React.useState(false);
+//JavaScript Part
+export default function Login() {
+    const [isVisible, setIsVisible] = React.useState(false);   
 
-    const toggleVisibility = () => setIsVisible(!isVisible);
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const submission = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log("handleSubmit");
+        console.log("Submission successful!");
     };
 
+    //HTML Part
     return (
+        //Tailwind CSS to center all elements. Login card and header CSS.
         <div className="flex h-full w-full items-center justify-center">
             <div className="rounded-large flex w-full max-w-sm flex-col gap-4">
                 <div className="flex flex-col items-center pb-6">
-                    <p className="text-xl font-medium">Welcome</p>
+                    <p className="text-3xl font-medium">Welcome</p>
                     <p className="text-small text-default-500">Log in to your account to continue</p>
                 </div>
-                <Form className="flex flex-col gap-3" validationBehavior="native" onSubmit={handleSubmit}>
+
+                <Form className="flex flex-col gap-3" validationBehavior="native" onSubmit={submission}>
+                    {/*Email Textfield*/}
                     <Input
                         isRequired
                         label="Email Address"
@@ -30,102 +33,24 @@ export default function Component() {
                         type="email"
                         variant="bordered"
                     />
+
+                    {/*Password Textfield*/}
                     <Input
                         isRequired
-                        endContent={
-                            <button type="button" onClick={toggleVisibility}>
-                                {isVisible ? (
-                                    <Icon
-                                        className="text-default-400 pointer-events-none text-2xl"
-                                        icon="solar:eye-closed-linear"
-                                    />
-                                ) : (
-                                    <Icon
-                                        className="text-default-400 pointer-events-none text-2xl"
-                                        icon="solar:eye-bold"
-                                    />
-                                )}
-                            </button>
-                        }
                         label="Password"
                         name="password"
                         placeholder="Enter your password"
-                        type={isVisible ? "text" : "password"}
+                        type="password"
                         variant="bordered"
                     />
-                    <div className="flex w-full items-center justify-between px-1 py-2">
-                        <Checkbox name="remember" size="sm">
-                            Remember me
-                        </Checkbox>
-                        <Link className="text-default-500" href="#" size="sm">
-                            Forgot password?
-                        </Link>
-                    </div>
+
+                    <br></br>
+                    {/*Login Button*/}
                     <Button className="w-full" color="primary" type="submit">
-                        Sign In
+                        Login
                     </Button>
                 </Form>
-                <div className="flex items-center gap-4 py-2">
-                    <Divider className="flex-1" />
-                    <p className="text-tiny text-default-500 shrink-0">OR</p>
-                    <Divider className="flex-1" />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <Button
-                        startContent={<Icon icon="flat-color-icons:google" width={24} />}
-                        variant="bordered"
-                    >
-                        Continue with Google
-                    </Button>
-                    <Button
-                        startContent={<Icon className="text-default-500" icon="fe:github" width={24} />}
-                        variant="bordered"
-                    >
-                        Continue with Github
-                    </Button>
-                </div>
-                <p className="text-small text-center">
-                    Need to create an account?&nbsp;
-                    <Link href="#" size="sm">
-                        Sign Up
-                    </Link>
-                </p>
             </div>
         </div>
     );
 }
-
-
-/*
-"use client";
-
-import { Input, Button } from "@heroui/react";
-import { useState } from "react";
-import "./login.css";
-
-export default function Login() {
-    //JavaScript Part
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const alertPopup = () => {alert("Successful!")};
-    
-    //HTML Part
-    return(
-        <div className="backgroundContainer">
-            <div className="cardContainer">
-                <h1 className="title">Login</h1>
-
-                <br></br>
-                <div className="infoContainer">
-                    <Input className="emailTextbox" placeholder="Email" type="email" onChange={(e) => setEmail(e.target.value)}/>
-                    <br></br> 
-                    <Input className="passwordTextbox" placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)}/>
-        
-                    <br></br>
-                    <Button className="loginButton" onClick={alertPopup}>Submit</Button>
-                </div>
-            </div>
-        </div>
-    );
-}*/
