ab1e6b2f1ff0233ea1caab682d7feb44e858951b
Joseph Bergmann <josephbergmann@Mac.adelphi.internal>
2025-12-02 13:57:26 -0500

    fixed issue with tickets table

--- backend/db/migrate/20251202185321_create_tickets.rb
+++ backend/db/migrate/20251202185321_create_tickets.rb
@@ -1,9 +1,9 @@
 class CreateTickets < ActiveRecord::Migration[8.1]
   def change
     create_table :tickets do |t|
-      t.string :title null: false
-      t.string :description null: false
-      t.string :status null: false
+      t.string :title, null: false
+      t.string :description, null: false
+      t.string :status, null: false
       t.string :category
       t.timestamps
     end
