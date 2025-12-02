f123f25d655733c7ff543e96699353cada5b1b0c
Joseph Bergmann <josephbergmann@Mac.adelphi.internal>
2025-12-02 14:22:36 -0500

    updated user model and created ticket model

---
+++ backend/app/models/ticket.rb
@@ -0,0 +1,4 @@
+class Ticket < ApplicationRecord
+    validates :title, presence: true, length: {maximum: 255}
+    validates :description, presence: true, length: {maximum: 1000}
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true
     validates :name, presence: true
-    validates :password, length: {minimum: 6}
+    validates :password, allow_nil: true, length: {minimum: 6}
+    validates :role, presence: true
+    validate :role_must_be_valid
+
+    private
+    def role_must_be_valid
+        roles = ["student", "faculty", "staff"]
+
+        unless roles.include?(role)
+            errors.add(:role, "must be one of #{roles.join(', ')}")
+        end
+    end
 end
