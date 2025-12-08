5ecd2c2ceace08f5c79959f8d8610a830a7f8f00
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2025-12-07 23:44:34 -0500

    added sessions routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-resources :tickets
-resources :users, only: [:index, :show]
+  namespace :api, defaults: {format: :json} do
+    resources :tickets
+    resources :users, only: [:index, :show]
+    resource :session, only: [:create, :show, :destroy]
+  end
 end
